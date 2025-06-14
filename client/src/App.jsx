c326573a67ee2ec1d150ed5de35115f582464375
Ayush Kumar <ayush4449kumar@gmail.com>
2025-06-14 23:46:46 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,120 @@
+import { useEffect, useState } from "react"
+import { Routes, BrowserRouter, Route, useNavigate, Navigate } from "react-router-dom"
+import Login from "./screens/Login"
+import { LoginContext } from "./contexts/LoginContext"
+import Home from "./screens/Home"
+import Signup from "./screens/Signup"
+import Profile from "./screens/Profile"
+import ProtectedRoutes from "./utils/ProtectedRoutes"
+import { getStoredUser, storeUser } from "./utils/localStorage"
+
+
+function App() {
+  const [user, setUser] = useState(false)
+  const [userdata, setUserdata] = useState(null)
+   const [authChecked, setAuthChecked] = useState(null)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const saved = getStoredUser();
+    if (saved) {
+      setUserdata(saved)
+      setUser(true)
+    };
+    setAuthChecked(true);
+  }, []);
+
+
+  const handleLogin = async (user) => {
+    try {
+      const res = await fetch("http://localhost:5000/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+      });
+
+      const data = await res.json();
+      if (data.success == "true") {
+        setUserdata(user)
+        storeUser(data.user)
+        setUser(true)
+        navigate("/profile")
+
+      }
+       else{
+        alert(`Error: ${data.error}`);
+      }
+
+
+    } catch (err) {
+      console.error('Login error:', err);
+      alert('Something went wrong.');
+    }
+
+
+  }
+
+  const createUser = async (user) => {
+    try {
+      const res = await fetch("http://localhost:5000/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+      });
+
+      const data = await res.json();
+      if (data.success == "true") {
+        setUserdata(user)
+        storeUser(data.user)
+        setUser(true)
+        navigate("/profile")
+
+      }
+      else{
+        alert(`Error: ${data.error}`);
+      }
+
+
+    } catch (err) {
+      console.error('Login error:', err);
+      alert('Something went wrong.');
+    }
+
+  }
+
+  const handleLogout=()=>{
+    localStorage.removeItem('authUser');
+    setUserdata(null)
+    setUser(true)
+    navigate("/login")
+  }
+
+  if (!authChecked) return null; // or loading spinner
+
+  return (
+    <>
+      <div className="flex w-screen h-full justify-center gap-2 bg-white">
+        <div className=" w-[375px] h-screen p-5 bg-[#F7F8F9] relative">
+
+          <LoginContext.Provider value={{ userdata ,user}}>
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="/login" element={user? <Navigate to={"/profile"}/>:<Login handleLogin={handleLogin} />} />
+              <Route path="/signup" element={user? <Navigate to={"/profile"}/>:<Signup createUser={createUser} />} />
+              <Route element={<ProtectedRoutes user={user} />}>
+                <Route path="/profile" element={<Profile handleLogout={handleLogout}/>} />
+              </Route>
+            </Routes>
+          </LoginContext.Provider>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
